b487b0f2f575a09aa790815ca7a52c90b4187328
James Molet <jmolet@redhat.com>
2012-10-10 16:25:29 -0400

    adding symlinks to setup

--- setup.zsh
+++ setup.zsh
@@ -1,7 +1,20 @@
 #!/bin/zsh
 
+DEST="${ZDOTDIR:-$HOME}/.home"
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^(README.md|zshrc|zpreztorc)(.N); do
   echo $rcfile
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+ln -s "$DEST/zshrc" "${ZDOTDIR:-$HOME}/.zshrc"
+ln -s "$DEST/zpreztorc" "${ZDOTDIR:-$HOME}/.zpreztorc"
+ln -s "$DEST/vimrc" "${ZDOTDIR:-$HOME}/.vimrc"
+#ln -s "$DEST/emacs.d" "${ZDOTDIR:-$HOME}/.emacs.d"
+
+echo "done"
+echo
+echo "Now run 'chsh -s' to change your used shell to activate settings"
